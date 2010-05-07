430cfaa0ac7db4bf6a828679adbd643022087c8d
Bill Tozier <bill@vagueinnovation.com>
2010-05-07 10:23:52 -0400

    rewriting Factory#initialize to handle three tiers of argument defaults

--- lib/factories/factory.rb
+++ lib/factories/factory.rb
@@ -3,50 +3,60 @@ module AnswerFactory
     require 'open-uri'
     
     attr_reader :name
-    attr_reader :instruction_library, :type_library
+    attr_reader :nudge_instructions
+    attr_reader :nudge_types
+    attr_reader :couchdb_server
     attr_accessor :workstation_names
     attr_reader :original_options_hash
     
     
-    def initialize(name = "my_factory", options = {})
-      @name = name
-      @original_options_hash = options
-      @instruction_library = options[:instruction_library] || Instruction.all_instructions
-      @type_library = options[:type_library] || NudgeType.all_types
-      @workstation_names = Array.new
+    def initialize(options = {})
       
-      self.configure!
+      # Factory instance settings
+      @name = options[:name] ||
+        (configatron.factory.name unless configatron.factory.name.nil?) ||
+        "my_factory"
+      
+      @workstation_names = options[:workstation_names] ||
+              (configatron.factory.workstation_names unless configatron.factory.workstation_names.nil?) ||
+              Array.new
+      
+      # CouchDB settings
+      @couchdb_server = options[:couchdb_server] ||
+              (configatron.factory.couchdb.server unless configatron.factory.couchdb.server.nil?) ||
+              "http://127.0.0.1:5984"
+      
+      # Nudge language settings
+      @nudge_instructions = options[:nudge_instructions] ||
+        (configatron.nudge.instructions.all unless configatron.nudge.instructions.all.nil?) ||
+        Instruction.all_instructions
+      
+      @nudge_types = options[:nudge_types] ||
+          (configatron.nudge.types.all unless configatron.nudge.types.all.nil?) ||
+          NudgeType.all_types
+      
+      
+      
+      save_configuration!
+    end
+    
+    # this apparent redundancy saves project-based
+    # and command-line overrides
+    def save_configuration!
+      configatron.factory.name = @name
+      configatron.nudge.instructions.all = @nudge_instructions
+      configatron.nudge.types.all = @nudge_types
+      configatron.factory.workstation_names = @workstation_names
+      configatron.factory.couchdb.server = @couchdb_server
     end
     
     
     def couch_available?
-      open(self.configatron.couchdb_uri).status
+      open(configatron.couchdb_uri).status
       true
     rescue StandardError
       false 
     end
     
-    
-    def configure!
-      self.configure_constants!
-      self.configure_paths!
-      self.configure_databases!
-    end
-    
-    
-    def configure_constants!
-      configatron.factory_name = self.name
-    end
-    
-    
-    def configure_paths!
-      configatron.factory_root = File.expand_path("#{File.dirname(__FILE__)}/../..")
-    end
-    
-    
-    def configure_databases!
-      configatron.configure_from_yaml("#{self.configatron.factory_root}/config/database.yml")
-      configatron.couchdb_uri = "#{self.configatron.main_database.db_root}/#{self.name}"
-    end
   end
 end
--- spec/factories/factory_spec.rb
+++ spec/factories/factory_spec.rb
@@ -2,51 +2,114 @@ require File.join(File.dirname(__FILE__), "./../spec_helper")
 
 
 describe "Factory" do
-  
-  it "should have a name" do
-    Factory.new("foo_factory").name.should == "foo_factory"
-  end
-  
-  it "should have a default name of 'my_factory'" do
-    Factory.new.name.should == "my_factory"
-  end
-  
-  
   describe "Factory configuration methods" do
-    describe "configatron integration" do
-      it "should be possible to talk to to #configatron" do
+    describe "defaults, via configatron" do
+      it "should be possible to talk to to #configatron from here" do
         lambda{configatron.my_thing}.should_not raise_error
       end
-    end
-    
-    
-    describe "configure_paths" do
-      it "should respond to #configure_paths" do
-        Factory.new.should respond_to(:configure_paths!)
+      
+      describe "Factory.new should have reasonable default values" do
+        it "should use 'my_factory' for #name" do
+          f1 = Factory.new
+          f1.name.should == "my_factory"
+        end
+        
+        it "should use an empty Array for #workstation_names" do
+          f1 = Factory.new
+          f1.workstation_names.should == []
+        end
+        
+        it "should use 'http://127.0.0.1:5984' for #couchdb_server" do
+          f1 = Factory.new
+          f1.couchdb_server.should == 'http://127.0.0.1:5984'
+        end
+        
+        it "should use Nudge::Instruction.all_instructions for #nudge_instructions" do
+          f1 = Factory.new
+          f1.nudge_instructions.should == Instruction.all_instructions
+        end
+        
+        it "should use Nudge::NudgeType.all_types for #nudge_types" do
+          f1 = Factory.new
+          f1.nudge_types.should == NudgeType.all_types
+        end
       end
-
-      it "should populate self.configatron.factory_root as the path to the project folder" do
-        pending "Broken since moving these specs"
-        this_spec_file_parent_path = File.expand_path("#{File.dirname(__FILE__)}/..")
-        f1 = Factory.new
-        f1.configure_paths!
-        f1.configatron.factory_root.should == this_spec_file_parent_path
+      
+      
+      describe "Factory.new should hit configatron for default values" do
+        it "should hit configatron for #name" do
+          configatron.temp do
+            configatron.factory.name = "foo"
+            f1 = Factory.new
+            f1.name.should == "foo"
+          end
+        end
+        
+        it "should hit configatron for #nudge_instructions" do
+          configatron.temp do
+            configatron.nudge.instructions.all = [IntSubtractInstruction]
+            f1 = Factory.new
+            f1.nudge_instructions.should == [IntSubtractInstruction]
+          end
+        end
+        
+        it "should hit configatron for #nudge_types" do
+          configatron.temp do
+            configatron.nudge.types.all = [BoolType]
+            f1 = Factory.new
+            f1.nudge_types.should == [BoolType]
+          end
+        end
+        
+        it "should hit configatron for #workstation_names" do
+          configatron.temp do
+            configatron.factory.workstation_names = [:bar, :baz]
+            f1 = Factory.new
+            f1.workstation_names.should == [:bar, :baz]
+          end
+        end
+        
+        it "should hit configatron for #couchdb_server" do
+          configatron.temp do
+            configatron.factory.couchdb.server = "http://my.otherplace.com:7771"
+            f1 = Factory.new
+            f1.couchdb_server.should == "http://my.otherplace.com:7771"
+          end
+        end
+        
       end
-    end
-
-
-    describe "factory name" do
-      it "should populate self.configatron.factory_name" do
-        f1 = Factory.new
-        f1.configure_constants!
-        f1.configatron.factory_name.should == "my_factory"
-
-        f1 = Factory.new("super_fancy")
-        f1.configure_constants!
-        f1.configatron.factory_name.should == "super_fancy"
+      
+      
+      describe "writing overridden defaults to configatron" do
+        describe "Factory.new should overwrite configatron default values" do
+          it "should overwrite #factory_name" do
+            Factory.new(name:"golly")
+            configatron.factory.name.should == "golly"
+          end
+          
+          it "should overwrite #nudge.instructions.all" do
+            Factory.new(nudge_instructions:[BoolAndInstruction])
+            configatron.nudge.instructions.all.should == [BoolAndInstruction]
+          end
+          
+          it "should overwrite #nudge.types.all" do
+            Factory.new(nudge_types:[BoolType])
+            configatron.nudge.types.all.should == [BoolType]
+          end
+          
+          it "should overwrite #factory.workstation_names" do
+            Factory.new(workstation_names:[:xenon])
+            configatron.factory.workstation_names.should == [:xenon]
+          end
+          
+          it "should overwrite #factory.couchdb.server" do
+            Factory.new(couchdb_server:"http://127.0.0.1:9999")
+            configatron.factory.couchdb.server.should == "http://127.0.0.1:9999"
+          end
+        end
       end
+      
     end
-
 
     describe "database setup" do
       describe "paths" do
@@ -85,13 +148,13 @@ describe "Factory" do
   describe "databases" do
     describe "#couch_available?" do
       it "should have a method to check that couchDB is accessible" do
-        f1 = Factory.new("boo")
+        f1 = Factory.new(name:"boo")
         lambda{f1.couch_available?}.should_not raise_error
       end
       
       it "should return true if the uri is reachable" do
         uri = "http://mycouch.db/boo"
-        f1 = Factory.new("boo")
+        f1 = Factory.new(name:"boo")
         f1.configatron.couchdb_uri = uri
         FakeWeb.register_uri(:any, uri, :body => "We are here!", :status => [200, "OK"])
         f1.couch_available?.should == true
@@ -99,13 +162,13 @@ describe "Factory" do
       
       it "should return false if the uri is offline or 404's out" do
         uri = "http://mycouch.db/boo"
-        f1 = Factory.new("boo")
+        f1 = Factory.new(name:"boo")
         f1.configatron.couchdb_uri = uri
         f1.configatron.couchdb_uri.should == uri
         FakeWeb.register_uri(:any, uri, :body => "Go away!", :status => [404, "Not Found"])
         f1.couch_available?.should == false
         
-        f1 = Factory.new("boo") # depends on this being wrong
+        f1 = Factory.new(name:"boo") # depends on this being wrong
         f1.configatron.couchdb_uri = "http://127.0.0.1:9991/place"
         f1.couch_available?.should == false
       end
@@ -113,59 +176,6 @@ describe "Factory" do
   end
   
   
-  describe "build_from_config" do
-    it "should read the config files"
-    
-    it "should #reset"
-  end
-  
-  
-  describe "reset_from_config" do
-    it "should erase the couchdb"
-    
-    it "should set up a new couchdb"
-    
-    it "should set up the necessary design documents in the db"
-  end
-  
-  
-  describe "activate" do
-    it "should check the config files"
-  end
-  
-  
-  describe "ontology" do
-    it "should have a master Instruction list" do
-      Factory.new("foo").instruction_library.should == Instruction.all_instructions
-    end
-    
-    it "should be possible to override the default with an option" do
-      short_list = [IntAddInstruction, CodeNoopInstruction]
-      Factory.new("foo", instruction_library:short_list).instruction_library.should ==
-        short_list
-    end
-    
-    it "should have a master NudgeType list" do
-      Factory.new("foo").type_library.should == NudgeType.all_types
-    end
-    
-    it "should be possible to override the default with an option" do
-      short_list = [CodeType, BoolType]
-      Factory.new("foo", type_library:short_list).type_library.should ==
-        short_list
-    end
-    
-    it "should save all the Hash options it was called with" do
-      Factory.new("bar", my_option:1, my_other_option:[1,2,3]).original_options_hash.should ==
-        {:my_option=>1, :my_other_option=>[1, 2, 3]}
-    end
-    
-  end
-  
-  describe "workstations" do
-    it "should have a list of extant workstations"
-    
-  end
   
   
 end
