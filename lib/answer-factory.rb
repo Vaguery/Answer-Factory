99daf8926b63f0a6b21abdab27ac5730b24aae57
Bill Tozier <bill@vagueinnovation.com>
2010-05-08 09:27:33 -0400

    drastic measure: removed all operator code and specs, to be replaced with new revisions throughout

--- lib/answer-factory.rb
+++ lib/answer-factory.rb
@@ -9,23 +9,6 @@ require 'couchrest'
 require 'answers/answer'
 require 'answers/batch'
 
-require 'machines/infrastructure'
-
-require 'machines/all_duplicated_genomes_sampler'
-require 'machines/any_one_sampler'
-require 'machines/dominated_quantile_selector'
-require 'machines/most_dominated_subset_sampler'
-require 'machines/nondominated_subset_selector'
-require 'machines/point_crossover_operator'
-require 'machines/point_delete_operator'
-require 'machines/point_mutation_operator'
-require 'machines/program_point_count_evaluator'
-require 'machines/random_guess_operator'
-require 'machines/resample_and_clone_operator'
-require 'machines/resample_values_operator'
-require 'machines/test_case_evaluator'
-require 'machines/uniform_backbone_crossover_operator'
-
 require 'factories/factory'
 require 'factories/workstation'
 
--- lib/operators/any_one_sampler.rb
+++
@@ -1,7 +0,0 @@
-module AnswerFactory
-  class AnyOneSampler < Sampler
-    def generate(crowd)
-      result = Batch[crowd.sample]
-    end
-  end
-end
