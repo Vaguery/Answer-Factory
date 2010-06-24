74fb0359752a1b62cd57b451cd6b700e56a504de
Jesse Sielaff <jesse.sielaff@gmail.com>
2010-06-24 02:52:57 -0400

    Created MutatePoint machine.

--- activate.rb
+++ activate.rb
@@ -5,39 +5,43 @@ Factory.use(:data_mapper).set_database("127.0.0.1/factory_a")
 Factory::Log.stream = true
 
 Workstation::Nudge::Generator.new(:generator) do |w|
-  w.generate_random.number_of_answers = 40
-  w.generate_random.path[:to_recipient] = :shuffler, :sample_any_one
+  w.generate_random.number_of_answers = 256
+  w.generate_random.path[:to_recipient] = :breeder, :sample_any_one
 end
 
-Workstation.new(:shuffler) do |w|
+Workstation.new(:breeder) do |w|
   Machine::Nudge::SampleAnyOne.new(:sample_any_one, w) do |m|
-    m.path[:of_sampled_one] = :shuffler, :send_along
-    m.path[:of_rest] = :shuffler, :point_crossover
+    m.path[:of_sampled_one] = :breeder, :mutate_point
+    m.path[:of_rest] = :breeder, :point_crossover
   end
   
-  Machine.new(:send_along, w) do |m|
+  Machine::Nudge::MutatePoint.new(:mutate_point, w) do |m|
+    m.number_of_mutants = 128
+    m.path[:of_mutated] = :breeder, :point_crossover
+    m.path[:of_mutants] = :breeder, :splitter
+  end
+  
+  Machine.new(:splitter, w) do |m|
+    m.path[:back] = :breeder, :mutate_point
+    m.path[:forward] = :breeder, :point_crossover
+    
     def m.process (answers)
-      send_answers(answers, [:shuffler, :send_to_scrap])
+      send_answers(answers.slice!(0...(answers.length / 2)), path[:back])
+      send_answers(answers, path[:forward])
     end
   end
   
   Machine::Nudge::PointCrossover.new(:point_crossover, w) do |m|
-    m.number_of_child_pairs = 5
+    m.number_of_child_pairs = 64
     m.path[:of_parents] = :dead_parents
     m.path[:of_children] = :created_children
   end
   
-  Machine.new(:send_to_scrap, w) do |m|
-    def m.process (answers)
-      send_answers(answers, :scrapyard, :never_used)
-    end
-  end
-  
   w.schedule :sample_any_one,
-             :send_along,
+             [:mutate_point, "5x"],
+             :splitter,
              [:point_crossover, "10x"]
-             :send_to_scrap
 end
 
-Factory.schedule :generator, :shuffler
+Factory.schedule :generator, :breeder
 Factory.run(5)
---
+++ factory/machine/nudge/mutate_point.rb
@@ -0,0 +1,41 @@
+module Machine::Nudge
+  class MutatePoint < Machine
+    option :point_size_of_mutation => 10,
+           :number_of_mutants => 1
+    
+    path :of_mutated,
+         :of_mutants,
+         :of_unused
+    
+    def process (answers)
+      return if answers.empty?
+      
+      mutated = answers.shuffle!.pop
+      mutants = []
+      
+      @number_of_mutants.times do
+        tree = NudgePoint.from(mutated.blueprint)
+        
+        n_of_mutation = rand(tree_points ||= tree.points)
+        
+        mutation = NudgePoint.from(::Nudge.random(@point_size_of_mutation))
+        
+        if n_of_mutation == 0
+          tree = mutation
+        else
+          tree.replace_point_at(n_of_mutation, mutation)
+        end
+        
+        mutant = Answer.new(:blueprint => tree.to_script)
+        
+        mutants.push(mutant)
+      end
+      
+      Factory::Log.answers(:create, mutants)
+      
+      send_answer(mutated, path[:of_mutated])
+      send_answers(mutants, path[:of_mutants])
+      send_answers(answers, path[:of_unused])
+    end
+  end
+end
