0234dcd572927d2f10f889c73c97738fa23b9d9e
Jesse Sielaff <jesse.sielaff@gmail.com>
2010-07-01 11:44:03 -0400

    Devised a scheme for allowing changes to be made to workstations and machines in the middle of a factory run.

--- factory/factory.rb
+++ factory/factory.rb
@@ -4,12 +4,22 @@ class Factory
     self
   end
   
+  def Factory.file= (config_filename)
+    @config_file = config_filename
+    Factory.reconfigure!
+  end
+  
+  def Factory.reconfigure!
+    Object.instance_eval(File.read(@config_file))
+  end
+  
   def Factory.run (n = 1)
     Factory::Log.timer("Factory.run") do
       Signal.trap(:INT) {|x| Log.write("Factory.run was terminated prematurely."); raise Shutdown }
       
       n.times do
         @schedule.each do |item|
+          Factory.reconfigure!
           item.run
         end
       end
