983bbc1bd199871b7a04336c16b85b97fd250e1c
Jesse Sielaff <jesse.sielaff@gmail.com>
2010-08-04 04:50:05 -0400

    Replaced demo with automatic factory generator.

--- demo/command/init.rb
+++
@@ -1,6 +0,0 @@
-# encoding: UTF-8
-require File.expand_path('../../answer_factory.rb', File.dirname(__FILE__))
-require 'yaml'
-
-file = File.expand_path('../database.yaml', File.dirname(__FILE__))
-Factory.database YAML.parser.load(File.read(file))
