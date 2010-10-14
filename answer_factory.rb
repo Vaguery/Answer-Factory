18100deab726fbd60e6831143fe228ae8821da05
Jesse Sielaff <jesse.sielaff@gmail.com>
2010-10-14 15:21:36 -0400

    Fixed path.

--- answer_factory.rb
+++ answer_factory.rb
@@ -1,5 +1,5 @@
 # encoding: UTF-8
-require File.expand_path("../../Nudge/nudge", File.dirname(__FILE__))
+require File.expand_path("../Nudge/nudge", File.dirname(__FILE__))
 
 Dir.glob(File.expand_path("factory/*.rb", File.dirname(__FILE__))) {|file| require file }
 Dir.glob(File.expand_path("built-in/languages/*/*.rb", File.dirname(__FILE__))) {|file| require file }
